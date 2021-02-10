5445263279c09e9f0f7a51c3cff338449364af7c
Innocent <Innocentbuthelezi@icloud.com>
2021-02-09 18:22:50 -0800

    Project Completed

--- script.js
+++ script.js
@@ -1,31 +1,66 @@
-const quoteContainer = document.getElementById('quote-container')
-const quoteText = document.getElementById('quote')
-const authorText = document.getElementById('author')
-const twitterBtn = document.getElementById('twitter')
-const newQuoteBtn = document.getElementById('new-quote')
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-// Get Quote From API
-async function getQuote() {
-    const apiUrl = 'https://type.fit/api/quotes/?method=getQuote&lang=en&format=json';
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        let number = Math.floor(Math.random() * data.length);
-        let quote;
-        if (data[number].author === null) {
-            quote = data[number];
-            quote.author = "Unknown";
-        } else {
-            quote = data[number];
-        }
-        console.log(quote)
-    } catch (error) {
-        console.log('whoops, no quote', error);
+//  Show Loading Spinner
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading Spinner
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
     }
 }
 
+// Get Quote From API
+async function getQuote() {
+    loading();
+
+    const proxyUrl = 'https://glacial-ridge-78146.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce Font Size for Longer Quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote')
+        } else {
+            quoteText.classList.remove('long-quote')
+        }
+        quoteText.innerText = data.quoteText;
+        // Stop Loader, Show Quote
+        complete();
+    } catch (error) {
+        getQuote();
+        console.log('whoops,no quote', error);
+    }
+}
+
+// Tweet Quote
+
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners 
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
 
-// NEXT STEPS ARE TO APPEND QUOTES FROM API AND LOAD ON SCREEN
